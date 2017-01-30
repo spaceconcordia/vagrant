a9210c5bd5663b5fb323e0eb0ad164e9995e1625
Alessandro Power <alessandrojpower@gmail.com>
2017-01-30 19:01:00 +0000

    Added directory check.

--- build-kernel.sh
+++ build-kernel.sh
@@ -2,7 +2,9 @@
 # Clones the Linux kernel in /home/vagrant and builds it.
 
 cd ~
-git clone https://github.com/spaceconcordia/linux-4.4.16
+if [ ! -d "linux-4.4.16" ]; then
+	git clone https://github.com/spaceconcordia/linux-4.4.16
+fi
 cd linux-4.4.16
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- acme-arietta_defconfig
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- acme-arietta.dtb
