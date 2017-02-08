49f295efcb1521f08b81b1a44d1c598bd98e63f4
Alessandro Power <alessandrojpower@gmail.com>
2017-02-08 14:08:44 +0000

    git clone SpacecraftSoftware during provision.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,19 +4,9 @@ apt-get update
 apt-get upgrade
 apt-get install -y $(< /vagrant/packages.list)
 
-# Clone and install Google Test.
-if [ ! -d /usr/local/src/googletest ]; then
-	cd /usr/local/src
-	git clone https://github.com/google/googletest
-	cd googletest
-	mkdir build && cd build
-	cmake -DCMAKE_BUILD_TYPE=Release ..
-	make
-	make install
-else
-	cd /usr/local/src/googletest
-	git pull
-	cd build
-	make
-	make install
+# Clone the SpacecraftSoftware repo.
+mkdir -p /vagrant/space-concordia
+cd /vagrant/space-concordia
+if [ ! -d SpacecraftSoftware ]; then
+	git clone --recursive https://github.com/spaceconcordia/SpacecraftSoftware/
 fi
