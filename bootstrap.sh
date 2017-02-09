c083ef17459d35a3d16567845f929c77f4bad51b
Alessandro Power <alessandrojpower@gmail.com>
2017-02-09 06:51:28 -0800

    Modified apt update to be entirely non-interactive; moved packages to be install directly into bootstrap.sh.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 apt-get update
-apt-get upgrade
-apt-get install -y $(< /vagrant/packages.list)
+DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+
+apt-get install -y bc binutils-arm-linux-gnueabi build-essential check clang \
+clang-format clang-tidy cmake g++-arm-linux-gnueabi gcc-arm-linux-gnueabi \
+git htop libc6-armel-cross libc6-dev-armel-cross liblz4-tool libncurses5-dev \
+libncursesw5-dev linux-tools-common linux-tools-generic qemu tmux unzip \
+valgrind vim zip
 
 # Clone the SpacecraftSoftware repo.
 mkdir -p /vagrant/space-concordia
