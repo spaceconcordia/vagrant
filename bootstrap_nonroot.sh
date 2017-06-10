8601089ed7a8d2096e409cb7b56ada96ffd532b9
Alessandro Power <alessandrojpower@gmail.com>
2017-06-10 19:49:16 +0000

    removed googletest as submodule.

--- bootstrap_nonroot.sh
+++ bootstrap_nonroot.sh
@@ -2,18 +2,24 @@
 mkdir -p /vagrant/space-concordia
 cd /vagrant/space-concordia
 if [ ! -d SpacecraftSoftware ]; then
-	git clone --recursive https://github.com/spaceconcordia/SpacecraftSoftware/
+	git clone https://github.com/spaceconcordia/SpacecraftSoftware/
 fi
 cd -
 
-# We are no longer using Buildroot as a submodule so this command purges it from
-# the repo.
+# We are no longer using Buildroot or Googletest as submodules so this command
+# purges them from the repo.
 cd /vagrant/space-concordia/SpacecraftSoftware
 if [ -d buildroot ]; then
-	git submodule deinit buildroot
-	rm -rf buildroot
+	git submodule deinit -f buildroot
 	rm -rf .git/modules/buildroot
+	git rm -rf buildroot
 fi
+if [ -d googletest ]; then
+	git submodule deinit -f googletest
+	rm -rf .git/modules/googletest
+	git rm -rf googletest
+fi
+cd ~
 
 # gdb-dashboard.
 if [ ! -f ~/.gdbinit ]; then wget -q -P ~ git.io/.gdbinit; fi
