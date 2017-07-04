aac283eb21b456fb271f40a60acdc4896dfd0162
Alessandro Power <alessandrojpower@gmail.com>
2017-07-04 08:12:44 -0400

    Added Node and Yarn installation. Added mission control virtualenv. Added MissionControl repo.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,19 @@ apt-get install -y bc binutils-arm-linux-gnueabi build-essential check clang \
 clang-format clang-tidy cmake g++-arm-linux-gnueabi gcc-arm-linux-gnueabi \
 git htop libc6-armel-cross libc6-dev-armel-cross libgtest-dev liblz4-tool \
 libncurses5-dev libncursesw5-dev linux-tools-common linux-tools-generic tmux \
-unzip valgrind vim zip sqlite3 libsqlite3-dev libc6-i386 tree
+unzip valgrind vim zip sqlite3 libsqlite3-dev libc6-i386 tree python3-pip
+
+# Install Yarn.
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update && apt-get install -y yarn
+
+# Install node.
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt-get install -y nodejs
+
+# Install virtualenvwrapper.
+pip3 install virtualenvwrapper
 
 # Build the Googletest library.
 mkdir -p /usr/src/gtest/build
--- bootstrap_nonroot.sh
+++ bootstrap_nonroot.sh
@@ -6,6 +6,35 @@ if [ ! -d SpacecraftSoftware ]; then
 fi
 cd -
 
+# Clone the MissionControl repo.
+mkdir -p /vagrant/space-concordia
+cd /vagrant/space-concordia
+if [ ! -d MissionControl ]; then
+	git clone https://github.com/spaceconcordia/MissionControl
+fi
+cd -
+
+# Create global gitignore.
+if [ ! -f ~/.gitignore_global ]; then
+	echo ".idea/" >> ~/.gitignore_global
+	echo "*.swp" >> ~/.gitignore_global
+
+	git config --global core.excludesfile ~/.gitignore_global
+fi
+
+# Create the virtual environments.
+if [ ! -d ~/.virtualenvs/MC ]; then
+	VIRTUALENVWRAPPER_PYTHON='/usr/bin/python3'
+	export WORKON_HOME=$HOME/.virtualenvs
+	source /usr/local/bin/virtualenvwrapper.sh
+	mkvirtualenv MC
+
+	# Add activation commands to .bashrc.
+	echo "VIRTUALENVWRAPPER_PYTHON='/usr/bin/python3'" >> ~/.bashrc
+	echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc
+	echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+fi
+
 # gdb-dashboard.
 if [ ! -f ~/.gdbinit ]; then wget -q -P ~ git.io/.gdbinit; fi
 
